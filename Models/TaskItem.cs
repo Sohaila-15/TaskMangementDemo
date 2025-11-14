17dede2f98cd2476ae5abb58ddbf9c495812aa2b
Sohaila-15 <sohailaashraf1593@gmail.com>
2025-11-14 23:36:07 +0200

    Created WebAPI and TaskItems

---
+++ Models/TaskItem.cs
@@ -0,0 +1,9 @@
+namespace TaskMangementDemo.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }   
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
